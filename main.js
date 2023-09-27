ea43d2ee7103d5550ee662576a41f5a7a7451cf2
bryanaldridgejr <bryanaldridgejr@gmail.com>
2023-09-27 14:29:39 -0500

    push file

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ let deviceLongitude = null
 let deviceLatitude = null
 
 const myLocation = () => {
-  console.log("myLocation function called")
-  console.log("Long:" + deviceLongitude)
-  console.log("Lat:" + deviceLatitude)
+  //console.log("myLocation function called")
+  //console.log("Long:" + deviceLongitude)
+  //console.log("Lat:" + deviceLatitude)
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition)
   } else {
@@ -14,11 +14,11 @@ const myLocation = () => {
 }
 
 const showPosition = (coordinates) => {
-  console.log("showPosition function called")
+  //console.log("showPosition function called")
   deviceLongitude = coordinates.coords.longitude
   deviceLatitude = coordinates.coords.latitude
-  console.log("Long:" + deviceLongitude)
-  console.log("Lat:" + deviceLatitude)
+  //console.log("Long:" + deviceLongitude)
+  //console.log("Lat:" + deviceLatitude)
 
   displayCoordinates.innerHTML =  "Longitude:  " + deviceLongitude + "<br/>  Latitude: " + deviceLatitude;
 }
